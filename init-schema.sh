6d1fb1d8bd31c20c03209b0404376aaf574070b5
mdelia17 <marcodelia1900@gmail.com>
2021-07-05 15:56:59 +0200

    added new tables to init-schema.sh

--- init-schema.sh
+++ init-schema.sh
@@ -5,7 +5,11 @@ cqlsh -e "create keyspace if not exists dns with replication = {'class':'SimpleS
 create table if not exists dns.nameserver (address text primary key, packets_sent counter, packets_received counter);
 create table if not exists dns.domain (domain text primary key, requests counter);
 create table if not exists dns.info (type text primary key, packets counter);
+create table if not exists dns.cname (cname text primary key, count counter);
+create table if not exists dns.ns (ns text primary key, count counter);
 truncate dns.nameserver; 
 truncate dns.domain;
-truncate dns.info; 
+truncate dns.info;
+truncate dns.cname;
+truncate dns.ns;
 exit;"

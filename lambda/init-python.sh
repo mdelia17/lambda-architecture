980dda051187501b36c31acc89c192b275eefa2c
Gianluca Mariani <gianluca@MacBook-Pro-di-Gianluca.local>
2021-07-22 19:04:11 +0200

    fix

--- lambda/init-python.sh
+++ lambda/init-python.sh
@@ -4,3 +4,4 @@ docker exec -it hive-server apt-get update -y
 docker exec -it hive-server apt-get install -y python3
 docker exec -it namenode apt-get update -y
 docker exec -it namenode apt-get install -y python3
+docker exec -it spark-master pip3 install cassandra-driver
